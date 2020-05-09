7b532134c79c3669ef7aeda35e986b814a97e825
Uwe Temme <uwe@temme.nrw>
2020-05-09 23:08:35 +0200

    packed-lock.json changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    path = require ('path'),    
+    compass = require('gulp-compass');
 
 var coffeeScripts = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -20,10 +22,32 @@ gulp.task('coffee', function(done){
     done();
 	});
 
-gulp.task('js', function(done){
+gulp.task('js', gulp.series('coffee', function(done) {
     gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))    
+    .pipe(gulp.dest('builds/development/js'))
     done();
-}); 
+})); 
+
+gulp.task('compass', function() {
+  gulp.src('components/sass/style.scss')
+    .pipe(compass({     
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('watch', function(done) {
+    gulp.series('components/coffee/*', 'coffee');
+    done();
+});
+
+
+gulp.task('default', function(done) { 
+    gulp.series('coffee','js','compass') 
+    done();
+});
