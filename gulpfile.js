303321e3587ec03611d8c2218164a2b6692d1804
Uwe Temme <uwe@temme.nrw>
2020-05-09 15:05:35 +0200

    concat added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeScripts = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js',
+];
 
 gulp.task('coffee', function(done){
   gulp.src(coffeeScripts)
@@ -11,3 +18,10 @@ gulp.task('coffee', function(done){
 	.pipe(gulp.dest('components/scripts'))
     done();
 	});
+
+gulp.task('js', function(done){
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))    
+    done();
+}); 
